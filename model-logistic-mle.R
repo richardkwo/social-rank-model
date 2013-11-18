bcc41d1d15aedf7c5e719a1301f71ecfaeaa9f5e
richardkwo <richardkwo@gmail.com>
2013-11-17 21:23:04 -0500

    s.c.

--- model-logistic-mle.R
+++ model-logistic-mle.R
@@ -10,9 +10,19 @@ pars.estimate <- c(R.estimate, H.estimate)
 
 mle.optim.results <- optimx(par=pars.estimate, fn=log.likelihood.logistic.wrapped, 
                             gr=gradient.wrapped, 
-                            hess=NULL, method=c("BFGS", "CG"), itnmax=100, 
+                            hess=NULL, method=c("CG"), itnmax=100, 
                             control = list(trace=1, save.failures=TRUE, maximize=TRUE, REPORT=1), 
                             A=A)
 
+R.estimate <- as.numeric(mle.optim.results[1, 1:N])
+H.estimate <- as.numeric(mle.optim.results[1, (N+1):(2*N)])
+pars.estimate <- c(R.estimate, H.estimate)
+
+
 imagefile.name <- paste("logistic-mle-",Sys.Date(),".RData",sep="")
 save.image(paste("rdata/", imagefile.name, sep=""))
+
+print(paste("Optim over with ll=", log.likelihood.logistic(R.estimate, H.estimate, A)))
+print(paste("Image saved to", imagefile.name))
+
+
